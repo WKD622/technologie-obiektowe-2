c5933f4c366d0d7dbea01cc84ef4e57fdab0f91c
WKD622 <kuba.ziark@gmail.com>
2018-10-16 13:18:15 +0200

    lab3 ex2 end

---
+++ lab3/lab-gui-2/to-javafx-2/src/main/java/pl/edu/agh/iisg/to/javafx/cw2/presenter/TransactionEditDialogPresenter.java
@@ -0,0 +1,93 @@
+package pl.edu.agh.iisg.to.javafx.cw2.presenter;
+
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.text.ParseException;
+import java.time.format.DateTimeFormatter;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import javafx.util.converter.LocalDateStringConverter;
+import pl.edu.agh.iisg.to.javafx.cw2.model.Category;
+import pl.edu.agh.iisg.to.javafx.cw2.model.Transaction;
+
+public class TransactionEditDialogPresenter {
+
+	private Transaction transaction;
+
+	@FXML
+	private TextField dateTextField;
+
+	@FXML
+	private TextField payeeTextField;
+
+	@FXML
+	private TextField categoryTextField;
+
+	@FXML
+	private TextField inflowTextField;
+
+	private Stage dialogStage;
+
+	private boolean approved;
+
+	public void setDialogStage(Stage dialogStage) {
+		this.dialogStage = dialogStage;
+	}
+
+	public void setData(Transaction transaction) {
+		this.transaction = transaction;
+		updateControls();
+	}
+
+	public boolean isApproved() {
+		return approved;
+	}
+
+	@FXML
+	private void handleOkAction(ActionEvent event) {
+		if (isInputValid())
+			updateModel();
+	}
+
+	@FXML
+	private void handleCancelAction(ActionEvent event) {
+		dialogStage.close();
+	}
+
+	private boolean isInputValid() {
+		// TODO: implement
+		return true;
+	}
+
+	private void updateModel() {
+		String pattern = "yyyy-MM-dd";
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern);
+		LocalDateStringConverter converter = new LocalDateStringConverter(formatter, formatter);
+		DecimalFormat decimalFormatter = new DecimalFormat();
+		decimalFormatter.setParseBigDecimal(true);
+
+		transaction.setDate(converter.fromString(dateTextField.getText()));
+		try {
+			transaction.setInflow((BigDecimal) decimalFormatter.parse(inflowTextField.getText()));
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		transaction.setPayee(payeeTextField.getText());
+		transaction.setCategory(new Category(categoryTextField.toString()));
+	}
+
+	private void updateControls() {
+		String pattern = "yyyy-MM-dd";
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern);
+		LocalDateStringConverter converter = new LocalDateStringConverter(formatter, formatter);
+
+		dateTextField.setText(converter.toString(transaction.getDate()));
+		payeeTextField.setText(transaction.getPayee().toString());
+		categoryTextField.setText(transaction.getCategory().toString());
+		inflowTextField.setText(transaction.getInflow().toString());
+	}
+}
